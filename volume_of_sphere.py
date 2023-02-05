e6d21179ced043a77e2a541264169bb574e7465d
Max Diamond <mdiamond25@utexas.edu>
2023-02-05 10:18:29 -0600

    radii 30 and 40

---
+++ volume_of_sphere.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def calculate_volume_of_sphere(radius):
+    pi=3.14
+    volume=(4/3)*radius**3
+    return volume
+
+radius1= 30
+print(calculate_volume_of_sphere(radius1))
+
+radius2= 40
+print(calculate_volume_of_sphere(radius2))
+    
+
+
+# In[ ]:
+
+
+
+
